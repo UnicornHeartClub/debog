5b453806c761a548041802f248445273c6f1285b
Tom Lackemann <tommylackemann@gmail.com>
2020-02-15 16:30:22 -0500

    test: debog + asyncDebog

---
+++ src/__tests__/index.ts
@@ -0,0 +1,98 @@
+/** @format */
+
+import debog, { asyncDebog } from '../'
+
+/**
+ * Example class
+ *
+ * We purporsely define functions in different ways to ensure we properly wrap them all
+ */
+class ExampleClass {
+  noopLoop() {
+    let output = 0
+    for (let i = 0; i < 10; i++) {
+      output += i
+    }
+    return output
+  }
+
+  longLoop = () => {
+    let output = 0
+    for (let i = 0; i < 1000000; i++) {
+      output += i
+    }
+    return output
+  }
+
+  async examineMeaningOfLife() {
+    return new Promise(resolve => setTimeout(() => resolve(42), 10))
+  }
+}
+
+describe('debog', () => {
+  let log: jest.SpyInstance
+
+  beforeEach(() => {
+    // prevent actual console logs, we just care if it was called or not
+    log = jest.spyOn(console, 'log').mockImplementation(() => {})
+  })
+
+  afterEach(() => {
+    log.mockClear()
+  })
+
+  describe('debog', () => {
+    it('logs when a class method is invoked', () => {
+      @debog('noopLoop')
+      class TestClass extends ExampleClass {}
+
+      const example = new TestClass()
+      example.noopLoop()
+
+      expect(log).toHaveBeenCalled()
+    })
+
+    it('returns the original value of the method', () => {
+      @debog('noopLoop')
+      class TestClass extends ExampleClass {}
+
+      const example = new TestClass()
+      const power = example.noopLoop()
+
+      expect(power).toBe(45)
+    })
+
+    it('logs when a threshold is exceeded', () => {
+      @debog(1, 'longLoop')
+      class TestClass extends ExampleClass {}
+
+      const example = new TestClass()
+      example.longLoop()
+
+      expect(log).toHaveBeenCalled()
+    })
+
+    it('does not log when a threshold is not met', () => {
+      @debog(20, 'longLoop')
+      class TestClass extends ExampleClass {}
+
+      const example = new TestClass()
+      example.longLoop()
+
+      expect(log).not.toHaveBeenCalled()
+    })
+  })
+
+  describe('asyncDebog', () => {
+    it('logs when an async class method is invoked', async () => {
+      @asyncDebog('examineMeaningOfLife')
+      class TestClass extends ExampleClass {}
+
+      const example = new TestClass()
+      const life = await example.examineMeaningOfLife()
+
+      expect(log).toHaveBeenCalledTimes(1)
+      expect(life).toBe(42)
+    })
+  })
+})
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@
  *
  * @format
  */
+import now from 'performance-now'
+
 export default function debog(...params: [number | string, ...string[]]) {
   const checkThreshold = typeof params[0] === 'number'
   let threshold = 0
@@ -16,17 +18,55 @@ export default function debog(...params: [number | string, ...string[]]) {
         super(...args)
 
         for (let method of params) {
-          this[method] = this.time(this[method], method as string)
+          this[method] = this.__time(this[method], method as string)
         }
       }
 
-      private time = (method: Function, name: string) => async (...args: any[]) => {
-        const s = performance.now()
-        const result = await method(...args)
-        const t = performance.now() - s
+      // @FIXME this should be private or protected but tsc is throwing an error
+      __time = (method: Function, name: string) => (...args: any[]) => {
+        const s = now()
+
+        let result = method(...args)
+
+        const t = now() - s
         if (t >= threshold) {
           console.log('%c%s took %fms', 'color: red', name, t)
         }
+
+        return result
+      }
+    }
+  }
+}
+
+export function asyncDebog(...params: [number | string, ...string[]]) {
+  const checkThreshold = typeof params[0] === 'number'
+  let threshold = 0
+  if (checkThreshold) {
+    threshold = params.unshift()
+  }
+
+  return function TimerFactory<T extends { new (...args: any[]): {} }>(Target: T) {
+    return class Timed extends Target {
+      constructor(...args: any[]) {
+        super(...args)
+
+        for (let method of params) {
+          this[method] = this.__asyncTime(this[method], method as string)
+        }
+      }
+
+      // @FIXME this should be private or protected but tsc is throwing an error
+      __asyncTime = (method: Function, name: string) => async (...args: any[]) => {
+        const s = now()
+
+        let result = await method(...args)
+
+        const t = now() - s
+        if (t >= threshold) {
+          console.log('%c%s took %fms', 'color: red', name, t)
+        }
+
         return result
       }
     }
